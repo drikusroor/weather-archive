03dd119ee5327f7a19ebf990819d3c31c57b9dda
Drikus Roor <drikusroor@gmail.com>
2024-02-12 22:01:07 +0100

    Update weather data fetching and saving logic

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from datetime import datetime
 
 # get from env
 API_KEY = os.environ.get('WEATHER_API_KEY')
-CITY = os.environ.get('CITY')
-URL = f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
+CITIES = os.environ.get('CITIES')
+
+def make_url(city):
+    return f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
 
 def fetch_weather_data(url):
     response = requests.get(url)
@@ -15,7 +17,7 @@ def fetch_weather_data(url):
     else:
         return None
 
-def save_to_csv(data, filename='weather_data.csv'):
+def save_to_csv(data, filename):
     fieldnames = ['timestamp', 'city', 'temperature', 'weather']
     row = {
         'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
@@ -32,11 +34,17 @@ def save_to_csv(data, filename='weather_data.csv'):
         print(f'I/O error: {e}')
 
 def main():
-    weather_data = fetch_weather_data(URL)
-    if weather_data:
-        save_to_csv(weather_data)
-    else:
-        print('Failed to fetch weather data')
+
+    for CITY in CITIES.split('\n'):
+        URL = make_url(CITY)
+        weather_data = fetch_weather_data(URL)
+
+        if weather_data:
+            curr_year = datetime.now().year
+            filename = f'archive/{CITY}_{curr_year}.csv'
+            save_to_csv(weather_data, filename)
+        else:
+            print('Failed to fetch weather data for', CITY)
 
 if __name__ == '__main__':
     main()
